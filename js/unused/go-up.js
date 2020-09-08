8f2b7780a9ec2d2b136db9165345d10968f9bda8
moelody <yfsmallmoon@gmail.com>
2020-09-08 23:39:48 +0800

    auto commit

---
+++ js/unused/go-up.js
@@ -0,0 +1,10 @@
+/**
+ * Created by moelody on 2020/6/29.
+ */
+(function () {
+  /*添加图片top*/
+  var top_up =
+    "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/moelody/CDN/blog/up.gif' title='回到顶部' >";
+  /*添加到返回顶部按钮下*/
+  document.getElementById("go-up").innerHTML += top_up;
+})();
